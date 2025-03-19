72fd6c475e1622f2387820df6dc38214dbd00d97
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-03-18 21:00:33 -0500

    Initialize app

---
+++ converter.js
@@ -0,0 +1,14 @@
+const secondsText = document.getElementById('seconds');
+const outputText = document.getElementById('output');
+function convertSeconds() {
+    const seconds = secondsText.value;
+    const secondsRemaining = seconds % 60;
+    const minutes = Math.floor(seconds / 60);
+    const minutesRemaining = minutes % 60;
+    const hours = Math.floor(minutes / 60);
+    const hoursRemaining = hours % 24;
+    const days = Math.floor(hours / 24);
+    const daysRemaining = days % 7;
+    const weeks = Math.floor(days / 7);
+    outputText.textContent = `${weeks} weeks, ${daysRemaining} days, ${hoursRemaining} hours, ${minutesRemaining} minutes, ${secondsRemaining} seconds\n(${weeks} weeks or ${days} days or ${hours} hours or ${minutes} minutes or ${seconds} seconds)`;
+}
