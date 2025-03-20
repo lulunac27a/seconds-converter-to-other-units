8b79d8391a3006b85207f6b15b995245fd96b6d0
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-03-19 20:31:35 -0500

    Add comments

--- converter.js
+++ converter.js
@@ -1,14 +1,15 @@
-const secondsText = document.getElementById("seconds");
-const outputText = document.getElementById("output");
+const secondsText = document.getElementById("seconds"); //seconds text box
+const outputText = document.getElementById("output"); //output text area
 function convertSeconds() {
-  const seconds = parseInt(secondsText.value, 10);
-  const secondsRemaining = seconds % 60;
-  const minutes = Math.floor(seconds / 60);
-  const minutesRemaining = minutes % 60;
-  const hours = Math.floor(minutes / 60);
-  const hoursRemaining = hours % 24;
-  const days = Math.floor(hours / 24);
-  const daysRemaining = days % 7;
-  const weeks = Math.floor(days / 7);
-  outputText.textContent = `${weeks} weeks, ${daysRemaining} days, ${hoursRemaining} hours, ${minutesRemaining} minutes, ${secondsRemaining} seconds\n(${weeks} weeks or ${days} days or ${hours} hours or ${minutes} minutes or ${seconds} seconds)`;
+  //function to convert seconds to weeks, days, hours, minutes, and seconds (other units)
+  const seconds = parseInt(secondsText.value, 10); //parse number of seconds value
+  const secondsRemaining = seconds % 60; //seconds remaining of minute
+  const minutes = Math.floor(seconds / 60); //total number of minutes
+  const minutesRemaining = minutes % 60; //minutes remaining of hour
+  const hours = Math.floor(minutes / 60); //total number of hours
+  const hoursRemaining = hours % 24; //hours remaining of day
+  const days = Math.floor(hours / 24); //total number of days
+  const daysRemaining = days % 7; //days remaining of week
+  const weeks = Math.floor(days / 7); //total number of weeks
+  outputText.textContent = `${weeks} weeks, ${daysRemaining} days, ${hoursRemaining} hours, ${minutesRemaining} minutes, ${secondsRemaining} seconds\n(${weeks} weeks or ${days} days or ${hours} hours or ${minutes} minutes or ${seconds} seconds)`; //update output text with all time units
 }
